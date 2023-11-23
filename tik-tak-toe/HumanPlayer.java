585f7d0b4c1858b56f9170b278c941b7a2176d32
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2023-11-23 14:08:56 +0100

    Child class HumanPlayer second commit, unsure of changes

--- tik-tak-toe/HumanPlayer.java
+++ tik-tak-toe/HumanPlayer.java
@@ -4,6 +4,7 @@ public class HumanPlayer extends Player{
 
     public HumanPlayer(String name){
         super(name);
+        this.actionListener = actionListener;
 
     }
 
@@ -11,12 +12,27 @@ public class HumanPlayer extends Player{
     public int[][] getActiveCoordinates(){
         protected void makeMove() {
 
-            //if button empty -- int Array JButton ...
+            actionListener = new ActionListener;
+
+
+            //if makeMove != activeCoordinates but does == currentGameNmbr && currentRoundNmbr
+            //get private int currentGameNmbr
+            //get private int currentRoundNmbr;
             int row = actionListener.nextInt();
             int col = actionListener.nextInt();
 
+            if (e.getSource() == buttons[i][j] && buttons[i][j].getText().isEmpty()) {
+
+                coordinates[0] = i;
+                coordinates[1] = j;
+
+                buttons[i][j].setText("X"); //Anpassa "X" efter spelare
+            }
+
+            //if (actionListener != activeCoordinates,
+
         }
-        return super.activCoordinates;
+        return super.activeCoordinates;
     }
 
 }
