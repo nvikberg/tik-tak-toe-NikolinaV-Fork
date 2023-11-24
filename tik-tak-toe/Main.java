e56b41290260917fe8de23c69dcf7f5fad42890f
SammyBoyDeluxe <151546643+SammyBoyDeluxe@users.noreply.github.com>
2023-11-24 16:21:58 +0100

    Possibility to try HumanPlayergames added

--- tik-tak-toe/Main.java
+++ tik-tak-toe/Main.java
@@ -1,7 +1,27 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
 
         PlayingField ticTacToeBoard = new PlayingField();
+        Random rand = new Random();
+        int nmbrOfPlayers = rand.nextInt(1,2);
+        //So you can test with humanplayers we set it to 1 directly
+        nmbrOfPlayers = 2;
+        if(nmbrOfPlayers == 1 ){
+
+
+
+
+        }
+        else{
+
+            HumanPlayer player1 = new HumanPlayer("Namn1");
+            HumanPlayer player2 = new HumanPlayer("Namn2");
+
+
+        }
+
     }
 
 }
--- tik-tak-toe/PlayingField.java
+++ tik-tak-toe/PlayingField.java
@@ -74,48 +74,6 @@ public class PlayingField implements ActionListener {
         framePlayingField.setVisible(true);
         }
 
-    /*public void checkForLines(){
-
-    public void initializeGameBoard(){
-        int chooseNow = JOptionPane.showOptionDialog(null, "How many players?", "Players selection",
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-                new String[] {"One Player", "Two Players"}, "One Player");
-
-        boolean oneOrTwoPlayers = (chooseNow == JOptionPane.YES_OPTION);
-        //we initialize JOptionpane to set the players.
-        //we need to have 2 buttons, that says one or two players
-        //we put actionListeners that we couple into boolean, is one player game or no.
-        //it needs to be focues, look at the feature that people have it.
-        for (int i = 0; i < 3; i++){
-            for(int k=0;k< 3;k++){
-                buttonArray[i][k]=new JButton();
-                buttonArray[i][k].addActionListener(new ButtonListener("click")); //it should listen to whats happening or click
-                panelInnerPlayingField.add(buttonArray[i][k]);
-            }
-        }
-
-    public void checkForLines(){
-        if(checkbuttonArrayAsThreeSameButtons(
-                buttonArray[0][0], buttonArray[1][1], buttonArray[2][2] ||
-                        buttonArray[1][2], buttonArray[1][1], buttonArray[2][0];
-        ) //then gameEnds or something(?)
-        if(checkbuttonArrayAsThreeSameButtons(
-                buttonArray[0][0], buttonArray[0][1], buttonArray[0][2] ||
-                        buttonArray[1][1], buttonArray[1][1], buttonArray[1][2] ||
-                        buttonArray[2][0], buttonArray[2][1], buttonArray[2][2];
-        ) //then gameEnds or something(?)
-        if(checkbuttonArrayAsThreeSameButtons(
-                buttonArray[0][0], buttonArray[1][0], buttonArray[2][0] ||
-                        buttonArray[0][1], buttonArray[1][1], buttonArray[2][1] ||
-                        buttonArray[0][2], buttonArray[1][2], buttonArray[2][2]
-        ) //then gameEnds or something(?)
-    }
-
-    private boolean checkbuttonArrayAsThreeSameButtons(JButton J1, JButton J2, JButton J3){
-        return !J1.getText().equals("")&&
-        J1.getText().equals(J2.getText()) &&
-        J2.getText().equals(J3.getText());
-        }*/
 
 
         public JButton getB(){
