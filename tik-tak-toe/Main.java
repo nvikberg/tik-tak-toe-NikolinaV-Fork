04ab135e84edeee966c65f8fcad7c89f71811af8
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2023-11-24 16:40:55 +0100

    Main fcode
    Main class code first proper commit

--- tik-tak-toe/Main.java
+++ tik-tak-toe/Main.java
@@ -1,7 +1,36 @@
 public class Main {
     public static void main(String[] args){
-
+        int roundNmbr = 1;
+        /*When we implement the choice-buttons we neeeeeeeeeeeeeed to change this to 0*/
+        Player[] playerArray = new Player[2];
+        boolean gameIsOver = false;
+        int chosenNmbrOfPlayers = 2;
         PlayingField ticTacToeBoard = new PlayingField();
+
+        if(chosenNmbrOfPlayers == 1) {
+            HumanPlayer player1 = new HumanPlayer("namn");
+            ComputerPlayer player2 =new ComputerPlayer();
+
+
+
+        }
+        else{
+            HumanPlayer player1 = new HumanPlayer("namn1");
+            HumanPlayer player2 = new HumanPlayer("namn2");
+            playerArray = new Player[2];
+            playerArray[0] = player1;
+            playerArray[1] = player2;
+
+        }
+        while(!gameIsOver){
+
+            playerArray[roundNmbr % 2].makeMove();
+
+            roundNmbr++;
+
+        }
+
+
     }
 
 }
